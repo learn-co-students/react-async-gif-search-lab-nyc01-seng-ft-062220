88626c074df9658b6034c3e63e7a7a592019d5bd
vakas-786 <vakas0959@gmail.com>
2020-08-28 12:02:07 -0400

    Done.

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 class GifSearch extends React.Component {
 
+    // submitShit = (e) => {
+    //     this.props.SearchIteration(e)
+    // }
+
+    //use this instead of the new onChange function that Eli used
+
 
     render() {
 
@@ -9,6 +15,7 @@ class GifSearch extends React.Component {
             <div>
             <h3>Search</h3>
             <input onChange={(e) => this.props.searchIteration(e)} type="text" placeholder="Search" value={this.props.searchValue}/>
+            {/* do not need to use helper function this callsback for you */}
             </div>
         )
     }
