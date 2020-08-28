1e3ae43d428cc1ba4439e891891cb158fbc66bfe
Dmitry <axiumone@gmail.com>
2020-08-28 11:15:22 -0400

    Done.

---
+++ src/components/GifList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+class GifList extends React.Component {
+
+
+
+
+
+   
+
+
+    render () {
+        
+      let picArr = this.props.data.map(picObj => <li><img src={picObj.images.original.url} /></li>)
+
+        return (
+
+            <ul>
+                {picArr}
+                {/* {console.log(this.props.data)} */}
+            </ul>
+        )
+    }
+
+}
+
+export default GifList;
