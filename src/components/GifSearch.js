53de8923b48c017e19ab8b03674d073c83384ad5
dukhyunko93 <dukhyunko93@gmail.com>
2020-08-28 15:44:52 -0400

    Done.

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
 class GifSearch extends React.Component {
- 
+    
+    state ={
+        searchVal : ""
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault()
+        this.props.submitHandler(this.state.searchVal)
+        this.setState({searchVal: "" })
+    }
+
+    onChangeHelper = (e) => {
+        this.setState({ searchVal : e.target.value })
+    }
+
     render() {
         return (
-            <form onSubmit={this.props.submitHandler}>
-                <input type="text" onChange={this.props.onChangeHelper} value={this.props.searchVal}  name="searchVal" placeholder="search gifs here"/>
+            <form onSubmit={this.submitHandler}>
+                <input type="text" onChange={this.onChangeHelper} value={this.state.searchVal}  name="searchVal" placeholder="search gifs here"/>
                 <input type="submit" value="Find Gifs"/>
             </form>
         )
--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -5,31 +5,24 @@ import GifSearch from '../components/GifSearch'
 class GifListContainer extends React.Component {
 
     state = {
-        firstThree: [],
-        searchVal : ""
+        firstThree: []
     }
 
     componentDidMount() {
-        fetch("https://api.giphy.com/v1/gifs/search?q=nba&api_key=tfFszbbifOLiUXE1fs8GUfv9oKLJ8eQs&rating=g")
-            .then(response => response.json())
-            .then(apiData => {
-                let shuffled = apiData.data.sort(() => 0.5 - Math.random()).slice(0,3)
-                this.setState({firstThree: shuffled}) 
-            })
+        this.fetchGifs()
     }
 
-   
-        submitHandler = (e) => {
-            e.preventDefault()
-            fetch(`https://api.giphy.com/v1/gifs/search?q=${this.state.searchVal}&api_key=tfFszbbifOLiUXE1fs8GUfv9oKLJ8eQs&rating=g`)
-            .then(response => response.json())
-            .then(apiData => this.setState({
-                firstThree: apiData.data.slice(0,3)
-            }))
-        }
-
-    onChangeHelper = (e) => {
-        this.setState({ [e.target.name] : e.target.value })
+    submitHandler = (searchTerm) => {
+        this.fetchGifs(searchTerm)
+    }
+    
+    fetchGifs = (term = "dolphins" ) => {
+        fetch(`https://api.giphy.com/v1/gifs/search?q=${term}&api_key=tfFszbbifOLiUXE1fs8GUfv9oKLJ8eQs&rating=g`)
+        .then(response => response.json())
+        .then(apiData => {
+            let shuffled = apiData.data.sort(() => 0.5 - Math.random()).slice(0,3)
+            this.setState({firstThree: shuffled}) 
+        })
     }
 
     render() {
@@ -37,7 +30,7 @@ class GifListContainer extends React.Component {
         <React.Fragment>
             {this.state.firstThree.length === 0 ? <h1>Hang on, Loading Gifs...</h1> : 
             <React.Fragment>
-                <GifSearch searchVal={this.state.searchVal} submitHandler={this.submitHandler} onChangeHelper={this.onChangeHelper} />
+                <GifSearch submitHandler={this.submitHandler} />
                 <h1>Giflistcontainer reporting for duty!</h1>
                 <GifList key={this.state.firstThree.id} gifData={this.state.firstThree}/>
             </React.Fragment>
